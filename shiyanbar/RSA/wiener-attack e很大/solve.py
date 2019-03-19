901dcf2e57e0601a716026533d84e629679c3bc5
key <1612730560@qq.com>
2019-03-19 18:00:30 +0800

    rsa

---
+++ shiyanbar/RSA/wiener-attack e很大/solve.py
@@ -0,0 +1,47 @@
+"""
+适用情况：e过大或过小。
+
+工具：https://github.com/pablocelayes/rsa-wiener-attack
+
+在e过大或过小的情况下，可使用算法从e中快速推断出d的值。详细的算法原理可以阅读：低解密指数攻击 。
+"""
+
+import ContinuedFractions, Arithmetic, RSAvulnerableKeyGenerator
+
+def hack_RSA(e,n):
+    '''
+    Finds d knowing (e,n)
+    applying the Wiener continued fraction attack
+    '''
+    frac = ContinuedFractions.rational_to_contfrac(e, n)
+    convergents = ContinuedFractions.convergents_from_contfrac(frac)
+
+    for (k,d) in convergents:
+
+        #check if d is actually the key
+        if k!=0 and (e*d-1)%k == 0:
+            phi = (e*d-1)//k
+            s = n - phi + 1
+            # check if the equation x^2 - s*x + n = 0
+            # has integer roots
+            discr = s*s - 4*n
+            if(discr>=0):
+                t = Arithmetic.is_perfect_square(discr)
+                if t!=-1 and (s+t)%2==0:
+                    print("Hacked!")
+                    return d
+
+# TEST functions
+
+def test_hack_RSA():
+    print("Testing Wiener Attack")
+    n = 0x92411fa0c93c1b27f89e436d8c4698bcf554938396803a5b62bd10c9bfcbf85a483bd87bb2d6a8dc00c32d8a7caf30d8899d90cb8f5838cae95f7ff5358847db1244006c140edfcc36adbdcaa16cd27432b4d50d2348b5c15c209364d7914ef50425e4c3da07612cc34e9b93b98d394b43f3eb0a5a806c70f06697b6189606eb9707104a7b6ff059011bac957e2aae9ec406a4ff8f8062400d2312a207a9e018f4b4e961c943dfc410a26828d2e88b24e4100162228a5bbf0824cf2f1c8e7b915efa385efeb505a9746e5d19967766618007ddf0d99525e9a41997217484d64c6a879d762098b9807bee46a219be76941b9ff31465463981e230eecec69691d1
+    e = 0x6f6b385dd0f06043c20a7d8e5920802265e1baab9d692e7c20b69391cc5635dbcaae59726ec5882f168b3a292bd52c976533d3ad498b7f561c3dc01a76597e47cfe60614f247551b3dbe200e2196eaa001a1d183886eeacddfe82d80b38aea24de1a337177683ed802942827ce4d28e20efef92f38f1b1a18c66f9b45f5148cceabfd736de8ac4a49e63a8d35a83b664f9f3b00f822b6f11ff13257ee6e0c00ca5c98e661ea594a9e66f2bd56b33d9a13f5c997e67a37fcf9a0c7f04d119fe1ba261127357e64a4b069aefed3049c1c1fe4f964fd078b88bedd064abea385cfebd65e563f93c12d34eb6426e8aa321033cfd8fe8855b9e74d07fe4f9d70de46f
+    print("(e,n) is (", e, ", ", n, ")")
+
+    hacked_d = hack_RSA(e, n)
+
+    print("d is", hacked_d)
+
+if __name__ == "__main__":
+    test_hack_RSA()
---
+++ shiyanbar/RSA/广播攻击 明文跟e相同/solve.py
@@ -0,0 +1,37 @@
+"""
+广播攻击
+适用情况：模数n、密文c不同，明文m、加密指数e相同。一般会是e=k，然后给k组数据
+
+使用不同的模数n，相同的公钥指数e加密相同的信息。就会得到多个(m^e) ==ci (mod ni)，将(m^e)视为一个整体M，这就是典型的中国剩余定理适用情况。按照本文的中国剩余定理小节容易求得m^e的值，当e较小时直接开e方即可，可使用gmpy2.iroot(M,e) 方法。
+"""
+import random
+import gmpy2
+import functools
+m = random.randint(0x100000000000, 0xffffffffffff)
+
+
+def CRT(mi, ai):
+    # mi,ai分别表示模数和取模后的值,都为列表结构
+    # Chinese Remainder Theorem
+    # lcm=lambda x , y:x*y/gcd(x,y)
+    # mul=lambda x , y:x*y
+    # assert(reduce(mul,mi)==reduce(lcm,mi))
+    # 以上可用于保证mi两两互质
+    assert (isinstance(mi, list) and isinstance(ai, list))
+    M = functools.reduce(lambda x, y: x * y, mi)
+    ai_ti_Mi = [a * (M // m) * gmpy2.invert(M // m, m) for (m, a) in zip(mi, ai)]
+    return functools.reduce(lambda x, y: x + y, ai_ti_Mi) % M
+
+
+e = 3
+n1 = 0x43d819a4caf16806e1c540fd7c0e51a96a6dfdbe68735a5fd99a468825e5ee55c4087106f7d1f91e10d50df1f2082f0f32bb82f398134b0b8758353bdabc5ba2817f4e6e0786e176686b2e75a7c47d073f346d6adb2684a9d28b658dddc75b3c5d10a22a3e85c6c12549d0ce7577e79a068405d3904f3f6b9cc408c4cd8595bf67fe672474e0b94dc99072caaa4f866fc6c3feddc74f10d6a0fb31864f52adef71649684f1a72c910ec5ca7909cc10aef85d43a57ec91f096a2d4794299e967fcd5add6e9cfb5baf7751387e24b93dbc1f37315ce573dc063ecddd4ae6fb9127307cfc80a037e7ff5c40a5f7590c8b2f5bd06dd392fbc51e5d059cffbcb85555
+n2 = 0x60d175fdb0a96eca160fb0cbf8bad1a14dd680d353a7b3bc77e620437da70fd9153f7609efde652b825c4ae7f25decf14a3c8240ea8c5892003f1430cc88b0ded9dae12ebffc6b23632ac530ac4ae23fbffb7cfe431ff3d802f5a54ab76257a86aeec1cf47d482fec970fc27c5b376fbf2cf993270bba9b78174395de3346d4e221d1eafdb8eecc8edb953d1ccaa5fc250aed83b3a458f9e9d947c4b01a6e72ce4fee37e77faaf5597d780ad5f0a7623edb08ce76264f72c3ff17afc932f5812b10692bcc941a18b6f3904ca31d038baf3fc1968d1cc0588a656d0c53cd5c89cedba8a5230956af2170554d27f524c2027adce84fd4d0e018dc88ca4d5d26867
+n3 = 0x280f992dd63fcabdcb739f52c5ed1887e720cbfe73153adf5405819396b28cb54423d196600cce76c8554cd963281fc4b153e3b257e96d091e5d99567dd1fa9ace52511ace4da407f5269e71b1b13822316d751e788dc935d63916075530d7fb89cbec9b02c01aef19c39b4ecaa1f7fe2faf990aa938eb89730eda30558e669da5459ed96f1463a983443187359c07fba8e97024452087b410c9ac1e39ed1c74f380fd29ebdd28618d60c36e6973fc87c066cae05e9e270b5ac25ea5ca0bac5948de0263d8cc89d91c4b574202e71811d0ddf1ed23c1bc35f3a042aac6a0bdf32d37dede3536f70c257aafb4cfbe3370cd7b4187c023c35671de3888a1ed1303
+c1 = pow(m, e, n1)
+c2 = pow(m, e, n2)
+c3 = pow(m, e, n3)
+
+m_decrypt = gmpy2.iroot(CRT([n1, n2, n3], [c1, c2, c3]), e)[0]
+
+
+assert m == m_decrypt
